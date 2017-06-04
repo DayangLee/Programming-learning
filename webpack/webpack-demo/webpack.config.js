2c76bcb179e96ec26420d640945338f42b2e3013
李大洋 <dayanglee2015@163.com>
2017-06-04 23:55:30 +0800

    Update

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -3,23 +3,36 @@ var htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     main: './src/script/main.js',
-    a: './src/script/a.js'
+    a: './src/script/a.js',
+    b: './src/script/b.js',
+    c: './src/script/c.js'
   },
   output: {
     path: path.resolve(__dirname,'./dist'),
-    filename: 'js/[name]-[chunkhash].js'
+    filename: 'js/[name]-[chunkhash].js',
+    publicPath: 'http://ljy.com/'
   },
   plugins: [
     new htmlWebpackPlugin({
-      filename: 'index.html',
+      filename: 'a.html',
       template: 'index.html',
-      inject: false,
-      title: 'hello webpack',
-      date: new Date(),
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true
-      }
+      inject: 'body',
+      title: 'this is a.html',
+      excludeChunks:['b','c']
+    }),
+    new htmlWebpackPlugin({
+      filename: 'b.html',
+      template: 'index.html',
+      inject: 'body',
+      title: 'this is b.html',
+      chunks: ['b']
+    }),
+    new htmlWebpackPlugin({
+      filename: 'c.html',
+      template: 'index.html',
+      inject: 'body',
+      title: 'this is c.html',
+      chunks: ['c']
     })
   ]
 }
