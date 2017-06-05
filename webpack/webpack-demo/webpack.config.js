55700eb9ee18012b49ae0e4e2d2e9071ebdf5a43
LJY <dayanglee2015@163.com>
2017-06-05 18:31:43 +0800

    update webpack

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -16,23 +16,23 @@ module.exports = {
     new htmlWebpackPlugin({
       filename: 'a.html',
       template: 'index.html',
-      inject: 'body',
+      inject: false,
       title: 'this is a.html',
       excludeChunks:['b','c']
     }),
     new htmlWebpackPlugin({
       filename: 'b.html',
       template: 'index.html',
-      inject: 'body',
+      inject: false,
       title: 'this is b.html',
-      chunks: ['b']
+      chunks: ['b','main']
     }),
     new htmlWebpackPlugin({
       filename: 'c.html',
       template: 'index.html',
-      inject: 'body',
+      inject: false,
       title: 'this is c.html',
-      chunks: ['c']
+      chunks: ['c','main']
     })
   ]
 }
