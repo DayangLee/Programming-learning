c2e0b14600767f2e93e79bac3248dde293a74181
LJY <dayanglee2015@163.com>
2017-07-20 17:27:30 +0800

    add Vuex

---
+++ Vue/Vuex/vuex-demo1/src/components/Count.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+    <hr>
+    <h3>{{ $store.state.count }}</h3> 
+    <P>
+      <button @click="$store.commit('add')">+</button>
+      <button @click="$store.commit('minus')">-</button>
+    </P>
+  </div>
+</template>
+
+<script>
+import store from '@/vuex/store'
+export default {
+  data() {
+    return {
+      msg: 'Hello Vuex!'
+    }
+  },
+  store
+}
+</script>
+
+<style>
+
+</style>
